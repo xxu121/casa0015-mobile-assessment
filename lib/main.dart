b00f1a11d1b1e98002a4af5e86e2d0d166e2a92e
xxu121 <146341729+xxu121@users.noreply.github.com>
2024-04-17 19:44:40 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -235,18 +235,25 @@ class ListViewState extends State<MyListView>{
       ],
     );
   }
-  void updateList(String s) {
-    // Extracting heart rate and average BPM from MQTT message
-    final heartRateMatch = RegExp(r"Heart Rate: (\d+\.\d+)").firstMatch(s);
-    final averageBPMMatch = RegExp(r"Average BPM: (\d+)").firstMatch(s);
-    
-    setState(() {
-      if (heartRateMatch != null) currentHeartRate = double.parse(heartRateMatch.group(1)!);
-      if (averageBPMMatch != null) averageBPM = double.parse(averageBPMMatch.group(1)!);
-      feeds.add(Text(s));
-    });
-  }
+  void updateList(String message) {
+  final heartRatePattern = RegExp(r"Heart Rate: (\d+\.\d+)");
+  final averageBPMPattern = RegExp(r"Average BPM: (\d+)");
+
+  final heartRateMatch = heartRatePattern.firstMatch(message);
+  final averageBPMMatch = averageBPMPattern.firstMatch(message);
+
+  setState(() {
+    if (heartRateMatch != null) {
+      currentHeartRate = double.parse(heartRateMatch.group(1)!);
+      print("Updated Heart Rate: $currentHeartRate");  // Debugging output
+    }
+    if (averageBPMMatch != null) {
+      averageBPM = double.parse(averageBPMMatch.group(1)!);
+      print("Updated Average BPM: $averageBPM");  // Debugging output
+    }
+  });
 }
+
 
 
   Future<void> startMQTT() async{
@@ -264,39 +271,26 @@ class ListViewState extends State<MyListView>{
 
     // Connect the client, any errors here are communicated by raising of the appropriate exception.
     try {
-      await client.connect(username, password);
-    } catch (e) {
-      print('client exception - $e');
-      client.disconnect();
-    }
-
-    // Check we are connected
-    if (client.connectionStatus!.state == MqttConnectionState.connected) {
-      print('Mosquitto client connected');
-    } else {
-      print(
-          'ERROR Mosquitto client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
-      client.disconnect();
-    }
-
-    // Ok, lets try a subscription or two, note these may change/cease to exist on the broker
-    const topic = 'student/CASA0015/sensor/ucfnxxu/outTopic';
-    client.subscribe(topic, MqttQos.atMostOnce);
-
-
-    // The client has a change notifier object(see the Observable class) which we then listen to to get
-    // notifications of published updates to each subscribed topic.
-    client.updates!.listen( (List<MqttReceivedMessage<MqttMessage?>>? c) {
-      final receivedMessage = c![0].payload as MqttPublishMessage;
-      final messageString = MqttPublishPayload.bytesToStringAsString(receivedMessage.payload.message);
-
-      /// The payload is a byte buffer, this will be specific to the topic
-      print('Change notification:: topic is <${c[0].topic}>, payload is <-- $messageString -->');
-      print('');
-
-      //updateList(messageString);
-    } );
-
+    await client.connect(username, password);
+  } catch (e) {
+    print('Client exception - $e');
+    client.disconnect();
+    return;
   }
 
-//}
+  if (client.connectionStatus!.state == MqttConnectionState.connected) {
+    print('Mosquitto client connected');
+    client.subscribe('student/CASA0015/sensor/ucfnxxu/outTopic', MqttQos.atMostOnce);
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      if (c != null) {
+        final MqttPublishMessage message = c[0].payload as MqttPublishMessage;
+        final String messageString = MqttPublishPayload.bytesToStringAsString(message.payload.message);
+        updateList(messageString);
+      }
+    });
+  } else {
+    print('ERROR: Mosquitto client connection failed - disconnecting, state is ${client.connectionStatus!.state}');
+    client.disconnect();
+  }
+  }
+}
