a71e0001f4eac48e4c4a4a0e19ee83caec6b4d24
xxu121 <146341729+xxu121@users.noreply.github.com>
2024-04-21 00:30:41 +0100

    Update heartrate_secrets.h

--- heartrate_0015/heartrate_secrets.h
+++ heartrate_0015/heartrate_secrets.h
@@ -1,4 +1,4 @@
 #define SECRET_SSID "xxx"
 #define SECRET_PASS "xxx"
-#define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+#define SECRET_MQTTUSER "xxx"
+#define SECRET_MQTTPASS "xxx"
